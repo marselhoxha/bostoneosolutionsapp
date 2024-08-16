c1fe4aa35dd71c3e9e597372fb2cfcb3298dfc4e
Marsel Hoxha <marsel.hox@gmail.com>
2024-08-15 21:06:02 -0400

    Implemented toggle mfa

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -84,6 +84,14 @@ export class UserService {
         catchError(this.handleError)
       );
 
+  toggleMfa$ = () => <Observable<CustomHttpResponse<Profile>>>
+    this.http.patch<CustomHttpResponse<Profile>>
+      (`${this.server}/user/togglemfa`, {})
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );    
+
   private handleError(error: HttpErrorResponse): Observable<never> {
     console.log(error);
     let errorMessage: string;
