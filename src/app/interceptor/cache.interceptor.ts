29f61c107882e9b3b6aef59e2f6ca48e914b3c47
Marsel Hoxha <marsel.hox@gmail.com>
2024-08-22 17:35:37 -0400

    Added HTTP In Memory Cache

---
+++ src/app/interceptor/cache.interceptor.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { BehaviorSubject, Observable, of, Subject, tap, throwError } from 'rxjs';
+import { HttpCacheService } from '../service/http.cache.service';
+
+@Injectable()
+export class CacheInterceptor implements HttpInterceptor {
+  
+
+  constructor(private httpCache: HttpCacheService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> | Observable<HttpResponse<unknown>>{
+    if(request.url.includes('verify') || request.url.includes('login') || request.url.includes('register') 
+            || request.url.includes('refresh') || request.url.includes('resetpassword')) {
+          return next.handle(request);
+      }
+
+      if(request.method !== 'GET' || request.url.includes('download')) {
+          this.httpCache.evictAll();
+          return next.handle(request);
+      }
+
+      const cachedResponse: HttpResponse<any> = this.httpCache.get(request.url);
+      if(cachedResponse) {
+          console.log('Found Response in Cache', cachedResponse);
+          this.httpCache.logCache();
+          return of(cachedResponse);
+      }
+      return this.handleRequestCache(request, next);
+    
+  }
+
+  private handleRequestCache(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap(response => {
+        if(response instanceof HttpResponse && request.method !== 'DELETE') {
+          console.log('Caching Response', response);
+          this.httpCache.put(request.url, response);
+        }
+      })
+    );
+  }
+
+  
+}
---
+++ src/app/service/http.cache.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+
+
+@Injectable({ providedIn: 'root' })
+export class HttpCacheService {
+
+     private httpResponseCache: { [key: string]: HttpResponse<any> } = {};
+
+     put = (key: string, httpResponse: HttpResponse<any>): void => {
+
+        console.log('Caching response', httpResponse);
+        this.httpResponseCache[key] = httpResponse;
+     }
+
+     get = (key: string): HttpResponse<any>  | null | undefined => this.httpResponseCache[key];
+
+     evict = (key: string): boolean => delete this.httpResponseCache[key];
+
+     evictAll = (): void => {
+        console.log('Clearing entire cache');
+        this.httpResponseCache = {};
+     }
+
+     logCache = (): void => {
+        console.log('Cache:', this.httpResponseCache);
+     }
+
+    
+}
